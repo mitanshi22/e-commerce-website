ca24e1078016ff25913faf9f41167afb1a4eadbb
mitanshi22 <mitanshiyadav56@gmail.com>
2024-07-01 17:57:47 +0530

    modified.2

--- func.js
+++ func.js
@@ -5,6 +5,8 @@ let blogPage = document.querySelector(".trends");
 let mainPage = document.querySelector(".main");
 let abt = document.querySelector(".about");
 let cont = document.querySelector(".contact");
+let crt = document.querySelector(".cart");
+let acc = document.querySelector(".account-page");
 
 function homes(){
     mainPage.style.display="flex";
@@ -13,12 +15,14 @@ function homes(){
     prc2.style.display="block";
     abt.style.display="none";
     cont.style.display="none";
+    acc.style.display="none";
 
     document.getElementById("blog").style.color="black";
     document.getElementById("shop").style.color="black";
     document.getElementById("home").style.color="rgb(6, 147, 147)";
     document.getElementById("about").style.color="black";
     document.getElementById("contact").style.color="black";
+    document.getElementById("account").style.color="black";
 }
 
 function shops(){
@@ -28,12 +32,14 @@ function shops(){
     prc2.style.display="block";
     abt.style.display="none";
     cont.style.display="none";
+    acc.style.display="none";
 
     document.getElementById("blog").style.color="black";
     document.getElementById("shop").style.color="rgb(6, 147, 147)";
     document.getElementById("home").style.color="black";
     document.getElementById("about").style.color="black";
     document.getElementById("contact").style.color="black";
+    document.getElementById("account").style.color="black";
 }
 
 function blogs(){
@@ -43,12 +49,14 @@ function blogs(){
     blogPage.style.display="block";
     abt.style.display="none";
     cont.style.display="none";
+    acc.style.display="none";
 
     document.getElementById("blog").style.color="rgb(6, 147, 147)";
     document.getElementById("shop").style.color="black";
     document.getElementById("home").style.color="black";
     document.getElementById("about").style.color="black";
     document.getElementById("contact").style.color="black";
+    document.getElementById("account").style.color="black";
 }
 
 function abouts(){
@@ -58,12 +66,14 @@ function abouts(){
     blogPage.style.display="none";
     abt.style.display="block";
     cont.style.display="none";
+    acc.style.display="none";
 
     document.getElementById("blog").style.color="black";
     document.getElementById("shop").style.color="black";
     document.getElementById("home").style.color="black";
     document.getElementById("about").style.color="rgb(6, 147, 147)";
     document.getElementById("contact").style.color="black";
+    document.getElementById("account").style.color="black";
 }
 
 function contacts(){
@@ -73,12 +83,31 @@ function contacts(){
     blogPage.style.display="none";
     abt.style.display="none";
     cont.style.display="block";
+    acc.style.display="none";
 
     document.getElementById("blog").style.color="black";
     document.getElementById("shop").style.color="black";
     document.getElementById("home").style.color="black";
     document.getElementById("about").style.color="black";
     document.getElementById("contact").style.color="rgb(6, 147, 147)";
+    document.getElementById("account").style.color="black";
+}
+
+function accounts(){
+    mainPage.style.display="none";
+    prc.style.display="none";
+    prc2.style.display="none";
+    blogPage.style.display="none";
+    abt.style.display="none";
+    cont.style.display="none";
+    acc.style.display="block";
+
+    document.getElementById("blog").style.color="black";
+    document.getElementById("shop").style.color="black";
+    document.getElementById("home").style.color="black";
+    document.getElementById("about").style.color="black";
+    document.getElementById("contact").style.color="black";
+    document.getElementById("account").style.color="rgb(6, 147, 147)";
 }
 
 function show(img){
@@ -92,12 +121,28 @@ function show(img){
    blogPage.style.display="none";
    abt.style.display="none";
    cont.style.display="none"
-    document.querySelector(".prc").style.display="flex";
+    document.querySelector(".cart").style.display="flex";
 }
 
 function addCart(){
     alert("Added To Cart");
     location.reload()
+}
+
+var loginForm = document.getElementById("loginForm");
+var regForm = document.getElementById("regForm");
+var indi = document.getElementById("indicator");
+
+function register(){
+    regForm.style.transform="translateX(0px)";
+    loginForm.style.transform="translateX(0px)";
+    indi.style.transform="translateX(100px)";
+}
+
+function login(){
+    regForm.style.transform="translateX(300px)";
+    loginForm.style.transform="translateX(300px)";
+    indi.style.transform="translateX(0px)";
 }
 
  
